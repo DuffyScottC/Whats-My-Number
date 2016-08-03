cbf48199f8fb37ae063f1c4f18461fedde9f96c0
Scott Duffy <duffyscottc@gmail.com>
2016-08-02 20:40:22 -0400

    First running of the project to save the object.

--- Whats My Number/ViewController.swift
+++ Whats My Number/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var phoneNumber = "4844666798"
+        
+        UserDefaults.standard.set(phoneNumber, forKey: "number")
+        
+        var phoneNumberObject = UserDefaults.standard.object(forKey: "number")
+        
+        if let myPhoneNumber = phoneNumberObject as? String {
+            
+            print(myPhoneNumber)
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
